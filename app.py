638b553f4741ca4565ea85ee81abd425e4fe293d
barrymun <barrymun@tcd.ie>
2017-02-11 01:37:01 +0000

    working pml file selection and loading - files loaded (added) to a mongodb

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 # Python 2.7 required
 # Use "source venv/bin/activate"
 
-from flask import Flask, Markup, render_template
-from flask_bootstrap import Bootstrap
-from Tkinter import Tk
-from flask import request
-from tkFileDialog import askopenfilename
-from flask.ext.pymongo import PyMongo
-
 import md5
-import webbrowser
 import os
+
+from flask import Flask, render_template
+from flask import request
+from flask_bootstrap import Bootstrap
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 mongo = PyMongo(app)
@@ -19,35 +16,47 @@ Bootstrap(app)
 
 @app.route('/select-files', methods=['POST'])
 def handle_selected_files():
-    selected_files = request.form
+    db = mongo.db.dist
     files = request.form.getlist('check')
-    return render_template('selected.html', files=files)
+    for file in files:
+        current_file = db.files.find_one({'name': file})
+        if (current_file):
+            path = current_file['path']
+            m = md5.new()
+            m.update(file)
+            db.selected.insert({'name': file, 'path': path, 'id': m.hexdigest()})
+    return render_template('selected.html', selected_files=db.selected.find())
+
 
 @app.route('/')
 def index():
     db = mongo.db.dist
     return render_template('index.html', pml_files=db.files.find())
 
+
 # @param extension = ".pml"
 def load_pml_source_files(path, extension):
-    ext = (extension)
     db = mongo.db.dist
+    ext = (extension)
     for root, dirs, files in os.walk(path):
         for file in files:
             if (file.endswith(ext)):
-                if (db.files.find_one({'name':file})):
+                if (db.files.find_one({'name': file})):
                     continue
                 # search Mongo. If in Mongo, ignore. Otherwise, add.
                 file_path = os.path.join(root, file)
                 m = md5.new()
                 m.update(file)
-                db.files.insert({'name':file, 'path':file_path, 'id':m.hexdigest()})
-
+                db.files.insert({'name': file, 'path': file_path, 'id': m.hexdigest()})
 
 
 def setup():
+    db = mongo.db.dist
+    db.files.drop()
+    db.selected.drop()
     path = os.getcwd() + "/peos"
     load_pml_source_files(path, '.pml')
+
 
 if __name__ == '__main__':
     with app.app_context():
