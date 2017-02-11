d81fd3148be4985e30e846d3513c7e0885cd0beb
barrymun <barrymun@tcd.ie>
2017-02-11 21:58:22 +0000

    Load DINTO parts 1 and 2 complete, Report Analysis complete - dinto reporting needs more attention in app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import md5
 import os
 import subprocess
 
+import ontospy
 from flask import Flask, render_template
 from flask import request
 from flask_bootstrap import Bootstrap
@@ -19,6 +20,59 @@ process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 process.communicate()
 LOG_FILENAME = 'pml_logfile.log'
 logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+
+@app.route('/analyse-owl-files', methods=['POST'])
+def analyse_owl_selected_files():
+    db = mongo.db.dist
+    files = db.selectedowl.find()
+    for file in files:
+        name = file['name']
+        path = file['path']
+        model = ontospy.Ontospy(path)
+        model.classes
+        model.properties
+        model.toplayer
+        a_class = model.getClass("DINTO_000001")
+        a_class = a_class[0]
+        data = a_class.serialize()
+        # print(a_class.serialize())
+        m = md5.new()
+        m.update(name)
+        if (data != None):
+            db.analysisowl.insert({'name': name, 'path': path, 'process': data, 'id': m.hexdigest()})
+            logging.info('\n')
+            logging.info("Name = [ " + name + " ]")
+            logging.info("Path = [ " + path + " ]")
+            logging.info(data)
+        else:
+            db.analysisowl.insert(
+                {'name': name, 'path': path, 'process': "ERROR: no data present", 'id': m.hexdigest()})
+            logging.info('\n')
+            logging.info("Name = [ " + name + " ]")
+            logging.info("Path = [ " + path + " ]")
+            logging.info("ERROR: no data present")
+    return render_template('analyseowldinto.html', analyse_owl_files=db.analysisowl.find())
+
+
+@app.route('/select-owl-files', methods=['POST'])
+def handle_selected_owl_files():
+    db = mongo.db.dist
+    owlfiles = request.form.getlist('checkowl')
+    for file in owlfiles:
+        current_file = db.owlfiles.find_one({'name': file})
+        if (current_file):
+            path = current_file['path']
+            m = md5.new()
+            m.update(file)
+            db.selectedowl.insert({'name': file, 'path': path, 'id': m.hexdigest()})
+    return render_template('selectedowldinto.html', selected_owl_files=db.selectedowl.find())
+
+
+@app.route('/dinto-index', methods=['GET'])
+def dinto_index():
+    db = mongo.db.dist
+    return render_template('dintoindex.html', owl_files=db.owlfiles.find())
 
 
 @app.route('/analyse-files', methods=['POST'])
@@ -48,7 +102,6 @@ def analyse_selected_files():
             logging.info("Name = [ " + name + " ]")
             logging.info("Path = [ " + path + " ]")
             logging.info("ERROR: syntax error at input")
-
     return render_template('analyse.html', analyse_files=db.analysis.find())
 
 
@@ -66,10 +119,31 @@ def handle_selected_files():
     return render_template('selected.html', selected_files=db.selected.find())
 
 
-@app.route('/')
+@app.route('/index', methods=['GET'])
 def index():
     db = mongo.db.dist
     return render_template('index.html', pml_files=db.files.find())
+
+
+@app.route('/')
+def main():
+    return render_template('home.html')
+
+
+# @param extension = ".owl"
+def load_owl_source_files(path, extension):
+    db = mongo.db.dist
+    ext = (extension)
+    for root, dirs, owlfiles in os.walk(path):
+        for file in owlfiles:
+            if (file.endswith(ext)):
+                if (db.owlfiles.find_one({'name': file})):
+                    continue
+                # search Mongo. If in Mongo, ignore. Otherwise, add.
+                file_path = os.path.join(root, file)
+                m = md5.new()
+                m.update(file)
+                db.owlfiles.insert({'name': file, 'path': file_path, 'id': m.hexdigest()})
 
 
 # @param extension = ".pml"
@@ -93,8 +167,12 @@ def setup():
     db.files.drop()
     db.selected.drop()
     db.analysis.drop()
+    db.selectedowl.drop()
+    db.analysisowl.drop()
     path = os.getcwd() + "/peos"
     load_pml_source_files(path, '.pml')
+    dinto_path = os.getcwd()
+    load_owl_source_files(dinto_path, '.owl')
 
 
 if __name__ == '__main__':
